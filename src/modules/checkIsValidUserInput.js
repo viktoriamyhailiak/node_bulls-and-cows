ee757caa248da32d1a64c1109d8cb52244e92707
Viktoriia <viktoriamyhailiak@gmail.com>
2025-10-02 20:15:24 +0300

    add a solution

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -1,15 +1,18 @@
 'use strict';
 
-/**
- * Checks that the user input is valid.
- * Valid user input is a 4-digit number that does not start with 0
- * and does not contain any duplicate digits.
- *
- * @param {string} userInput - The user input
- * @return {boolean} - True if the user input is valid, false otherwise
- */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  const digitsSet = new Set(userInput);
+
+  if (
+    userInput.length !== 4 ||
+    userInput[0] === '0' ||
+    digitsSet.size !== 4 ||
+    !/^\d{4}$/.test(userInput)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 module.exports = {
